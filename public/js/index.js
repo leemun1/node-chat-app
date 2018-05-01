c622aa7ddf2a65ba32fac883ec2f06c7e2bbf1f9
Mike <munkyu0813@gmail.com>
2018-05-01 18:37:53 -0400

    integrate jQuery, add form for sending msgs and displaying incoming msgs in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,12 +10,20 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('New Message:', message);
+  let li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-  from: 'David',
-  text: 'Hi there'
-}, function (data) {
-  console.log('Got it.', data);
+$('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function () {
+
+  });
 });
 
