0f5f14da832cad1eef1916f4089456b13c2fb49f
Mike <munkyu0813@gmail.com>
2018-05-02 00:06:35 -0400

    format timestamps using moment.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
-
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app.'));
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
   socket.broadcast.emit('newUser');
