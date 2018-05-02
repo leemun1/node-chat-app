23375a264cb26f2f94ac38f77eec40c53e7c6fcc
Mike <munkyu0813@gmail.com>
2018-05-02 11:21:08 -0400

    change all room names to lowercase, preventing duplicate room names

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,7 +18,7 @@ function scrollToBottom () {
 
 socket.on('connect', function () {
   let params = $.deparam(window.location.search);
-
+  params.room = params.room.toLowerCase();
   socket.emit('join', params, function (error) {
     if (error) {
       alert(error);
